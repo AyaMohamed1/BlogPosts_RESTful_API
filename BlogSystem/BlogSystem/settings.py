62f6b92130b33f35bd7e7597a8e5478c8de5cf36
Aya Ibrahim <aya.ibrahim@itworx.com>
2023-04-10 23:08:34 +0200

    added render cloud service

--- BlogSystem/BlogSystem/settings.py
+++ BlogSystem/BlogSystem/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 import os
 
 load_dotenv()
@@ -29,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -79,15 +80,19 @@ WSGI_APPLICATION = 'BlogSystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
--- BlogSystem/Posts/urls.py
+++ BlogSystem/Posts/urls.py
@@ -4,23 +4,25 @@ from Posts.views import PostList, PostCreate, PostDetail, PostUpdate, PostDelete
         CategoryList, CategoryCreate, CategoryDetail, CategoryUpdate, CategoryDelete
 
 urlpatterns = [
-    path('', PostList.as_view()),
-    path('create/', PostCreate.as_view()),
-    path('<int:pk>/', PostDetail.as_view()),
-    path('<int:pk>/update/', PostUpdate.as_view()),
-    path('<int:pk>/delete/', PostDelete.as_view()),
-    path('authors/', AuthorList.as_view()),
-    path('authors/create/', AuthorCreate.as_view()),
-    path('authors/<int:pk>/', AuthorDetail.as_view()),
-    path('authors/<int:pk>/update/', AuthorUpdate.as_view()),
-    path('authors/<int:pk>/delete/', AuthorDelete.as_view()),
-    path('authors/<int:pk>/posts/', AuthorPosts.as_view()),
-    path('authors/posts/', AuthorsPosts.as_view()),
-    path('categories/', CategoryList.as_view()),
-    path('categories/create/', CategoryCreate.as_view()),
-    path('categories/<int:pk>/', CategoryDetail.as_view()),
-    path('categories/<int:pk>/update/', CategoryUpdate.as_view()),
-    path('categories/<int:pk>/delete/', CategoryDelete.as_view()),
+    path('', PostList.as_view(), name="listPosts"),
+    path('create/', PostCreate.as_view(), name="createPost"),
+    path('<int:pk>/', PostDetail.as_view(), name="getPost"),
+    path('<int:pk>/update/', PostUpdate.as_view(), name="updatePost"),
+    path('<int:pk>/delete/', PostDelete.as_view(), name="deletePost"),
+
+    path('authors/', AuthorList.as_view(), name="listAuthors"),
+    path('authors/create/', AuthorCreate.as_view(), name="createAuthor"),
+    path('authors/<int:pk>/', AuthorDetail.as_view(), name="getAuthor"),
+    path('authors/<int:pk>/update/', AuthorUpdate.as_view(), name="updateAuthor"),
+    path('authors/<int:pk>/delete/', AuthorDelete.as_view(), name="deleteAuthor"),
+    path('authors/<int:pk>/posts/', AuthorPosts.as_view(), name="getAuthorPosts"),
+    path('authors/posts/', AuthorsPosts.as_view(), name="getAuthorsPosts"),
+
+    path('categories/', CategoryList.as_view(), name="listCategories"),
+    path('categories/create/', CategoryCreate.as_view(), name="createCategory"),
+    path('categories/<int:pk>/', CategoryDetail.as_view(), name="getCategory"),
+    path('categories/<int:pk>/update/', CategoryUpdate.as_view(), name="updateCategory"),
+    path('categories/<int:pk>/delete/', CategoryDelete.as_view(), name="deleteCategory"),
     
 
 ]
